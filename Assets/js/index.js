a44eec15d5d082eab4f5303bf0485559018efb73
Jelani Thomas <jelani13@icloud.com>
2021-02-10 17:35:58 -0500

    JS incomplete

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,74 +1,8 @@
-// Quiz questions
-var questZombi  = [{
-    question: "Where do most of the Resident Evil games takes place?",
-    choices: ["Raccon City", "Chernobyl", "Midgar"],
-    correctAnswer: 1
-},  {
-    question: "What is Nemesis' signature saying?",
-    choices: ["Die", "Excelsior!", "S.T.A.R.S...."],
-    correctAnswer: 3
-}, {
-    question: "What is the name of the company of released the virus that caused the pandemic?",
-    choices: ["Umbrella", "WeylandUtani", "OCP"],
-    correctAnswer: 1
-},{
-    question: "What is the name of virus that causes the global pandemic?",
-    choices: ["COVID-19", "T-Virus", "Blacklight Virus"],
-    correctAnswer: 2
-},{
-    question: "What item is used in the original series to save your game?",
-    choices: ["Ink Ribbon", "Game Cartridge", "Camera"],
-    correctAnswer: 1
-},{
-    question: "What weapon is used to usually kill a boss in one hit?",
-    choices: ["Magnum", "Rocket Launcher", "Knife"],
-    correctAnswer: 2
-},{
-    question: "Resident Evil is a series based on____.",
-    choices: ["Demons", "Serial Killers", "Zombies"],
-    correctAnswer: 3
-},{
-    question: "_____is the name of a devloper behind Resident Evil.",
-    choices: ["Konami", "Capcom", "Square Enix"],
-    correctAnswer: 2
-},{
-    question: "In Resident Evil 3, Jill Valentine was constantly stalked and attacked by which B.O.W?",
-    choices: ["Mr.X", "Nemesis", "Thanos"],
-    correctAnswer: 1
-},{
-    question: "In Resident Evil 4, which handgun is available from the merchant for free after destroying 10-15 Blue Medallions?",
-    choices: ["Punisher", "Blacktail", "Red Nine"],
-    correctAnswer: 1
-},{
-    question: "In the live-action movie what is the name of the Supercomputer built to protect company assesrs",
-    choices: ["GlaDOS", "Red Queen", "HAL 9000",],
-    correctAnswer: 2
-},{
-    question: "Which Resident Evil game was the first one to be played from a first-person perspective?",
-    choices: ["Resident Evil 7:biohazrd", "Resident Evil:Code Veronica","Resident Evil:Umbrella Chronicles"],
-    correctAnswer: 1
-},{
-    question:"____is the name of the shades wearing, cold deameaner, murderous Umbrella company executive protagonist",
-    choices: ["Elle Woods", "Agent Smith", "Albert Wesker"],
-    correctAnswer: 3
-},{
-    question:"While the series is called Resident Evil in the west, in the east it's called _____",
-    choices: ["Infestation", "Dead Resistance", "Biohazard"],
-    correctAnswer: 3
-},{
-    question:"____ is the name of the scientist who developed the T-Virus",
-    choices:["Dr. James Marcus", "Dr. Charles Ashford", "Dr. Neil deGrasse Tyson"],
-    correctAnswer: 2
-}];
-
 // Query selectors to pull from HTML
+var rE_begin = document.querySelector(".load")
 var rE_question = document.querySelector(".question");
-var rE_info = document.querySelector(".credentials_info");
-var rE_doOver = document.querySelector(".restart");
-var rE_quit = document.querySelector(".die")
-var rE_box = document.querySelector(".quest_box");
-var rE_start = $("#lock_n_load");
-
-rE_start.on('click', function(){
-    alert("Survive the horde");
-});
+var rE_info = document.querySelector(".credentials");
+var rE_quit = document.querySelector(".die");
+var rE_restart = document.querySelector(".restart");
+var rE_quest = document.querySelector(".quest_box");
+var answ_option = document.querySelector("answ_list");
