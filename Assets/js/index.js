188410b816e555d0e2e5e8457d1552658dc38030
Jelani Thomas <jelani13@icloud.com>
2021-02-21 18:47:39 -0500

    edited question vars

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,8 +1,8 @@
 // Query selectors to pull from HTML
-var rE_begin = document.querySelector(".load")
+var rE_begin = document.querySelector(".start")
 var rE_question = document.querySelector(".question");
 var rE_info = document.querySelector(".credentials");
-var rE_quit = document.querySelector(".die");
+var rE_quit = document.querySelector(".quit");
 var rE_restart = document.querySelector(".restart");
 var rE_quest = document.querySelector(".quest_box");
 var answ_option = document.querySelector("answ_list");
